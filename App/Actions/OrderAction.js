a214761c5ffbfb10cc83a415654369d335e006e5
Franky Chen <Franky@Franky-2.hitronhub.home>
2018-07-27 16:39:45 -0400

    Fixed merged type

--- App/Actions/OrderAction.js
+++ App/Actions/OrderAction.js
@@ -1,6 +1,6 @@
 import CmDriverConstants from '../Constants/CmDriverConstants';
 import {dispatch, register} from '../Dispatchers/CmDriverDispatcher';
-import HistoryModule from '../Modules/HistoryModule/CmDriverHistoryModule';
+import OrderModule from '../Modules/OrderModule/OrderModule';
 import {NativeModules} from 'react-native';
 export default {
     async getOrders({lat, lng}){
@@ -12,7 +12,7 @@ export default {
           if (result.ev_error == 0) {
             const data = result.ev_orders;
             dispatch({
-                actionType: CmDriverConstants.GET_HISTORY, data
+                actionType: CmDriverConstants.GET_ORDERS, data
             })
           }
       }catch (e){
