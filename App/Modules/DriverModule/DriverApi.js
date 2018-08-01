399fad503685460a12d925f7f3acf4de69931cfb
Khalil <khalilleo@users.noreply.github.com>
2018-08-01 16:41:30 -0400

    Added timeout promise for orderList and updateGeoLocation APIs

--- App/Modules/DriverModule/DriverApi.js
+++ App/Modules/DriverModule/DriverApi.js
@@ -1,5 +1,28 @@
 import AppConstants from '../../Constants/AppConstants';
+
 export default {
+    _fetch(fetch_promise, timeout) {
+      var abort_fn = null;
+
+      //这是一个可以被reject的promise
+      var abort_promise = new Promise(function(resolve, reject) {
+             abort_fn = function() {
+                reject('abort promise');
+             };
+      });
+
+      //这里使用Promise.race，以最快 resolve 或 reject 的结果来传入后续绑定的回调
+       var abortable_promise = Promise.race([
+             fetch_promise,
+             abort_promise
+       ]);
+
+       setTimeout(function() {
+             abort_fn();
+        }, timeout);
+
+       return abortable_promise;
+    },
     updateDriverStatus(reqData) {
       // const url = 'https://www.cmapi.ca/cm_driver/dev/api/v1/driver_status';
       const url = AppConstants.API_DRIVER_STATUS;
@@ -24,7 +47,8 @@ export default {
               .catch((error) => {throw error});
     },
     updateGeolocation(reqData) {
-      // const url = 'https://www.cmapi.ca/cm_driver/dev/api/v1/geo_trace';
+      const TIMEOUT_SEC = 10;
+
       const url = AppConstants.API_GEO_TRACE;
       let options = {
           method: 'POST',
@@ -42,8 +66,13 @@ export default {
           geo_lat: reqData.geo_lat,
           geo_lng: reqData.geo_lng,
       });
-      return fetch(url,options)
-              .then((res) => res.json())
-              .catch((error) => {throw error});
+
+      return this._fetch(fetch(url, options), TIMEOUT_SEC * 1000)
+        .then(function(res) {
+            return res.json()
+        }, function(err) {
+            alert('服务器开小差了');
+            return null;
+        });
     }
 };
