d06bd537aaf9a83efb7347f373ed49d6518e73a1
ace68723 <ace68723@gmail.com>
2018-08-21 17:52:18 -0400

    release 1.5.1

--- App/Components/Login/Login.js
+++ App/Components/Login/Login.js
@@ -342,7 +342,7 @@ export default class MyComponent extends Component {
           <Text allowFontScaling={false}
               style={{ marginBottom: 5, fontFamily:'FZZhunYuan-M02S' }}
           >
-            v1.5.0
+            v1.5.1
           </Text>
           <Text allowFontScaling={false}
               style={{ fontSize: 11, fontFamily:'FZZhunYuan-M02S'}}
