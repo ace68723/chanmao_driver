f56007e1db59e245a91cee117cb13842916b72f6
Khalil <khalilleo@users.noreply.github.com>
2018-10-16 16:05:03 -0400

    Fix & Optimized CodePush update flow

--- iosApp.js
+++ iosApp.js
@@ -1,92 +1,96 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Image,
-  PushNotificationIOS,
-    Platform,
-  View,
-} from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry, Image, PushNotificationIOS, Platform, View} from 'react-native';
 import App from './App/App';
 import CodePush from "react-native-code-push";
-// const Realm = require('realm');
-// const realm = new Realm();
-import { getRealm } from './App/Modules/AuthModule/Auth';
+
+import {getRealm} from './App/Modules/AuthModule/Auth';
 const realm = getRealm();
-if (Platform.OS==='ios'){
-setTimeout(() => {
+
+const DEPLOYMENT_KEY = 'C_xLtYebjYVDRqviZOkIXHlHvkktH1TTPtmiQ';
+
+if (Platform.OS === 'ios') {
+  setTimeout(() => {
     PushNotificationIOS.requestPermissions();
     PushNotificationIOS.addEventListener('register', (deviceToken) => {
-        console.log(deviceToken);
-        realm.write(() => {
-            realm.create('AppUserInfo', { param: 'deviceToken', value: deviceToken }, true);
-        });
+      console.log(deviceToken);
+      realm.write(() => {
+        realm.create('AppUserInfo', {
+          param: 'deviceToken',
+          value: deviceToken
+        }, true);
+      });
     });
     PushNotificationIOS.addEventListener('registrationError', (error) => {
-        console.log('here');
-        console.log(error);
+      console.log('here');
+      console.log(error);
     });
     PushNotificationIOS.addEventListener('notification', (notification) => {
-        console.log(notification);
-
-        console.log('get data', notification.getData());
+      console.log(notification);
+      console.log('get data', notification.getData());
     });
-}, 500);
+  }, 500);
 }
 export default class cmDriver extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
-      isUpdate:false,
+      isUpdating: true
     }
   }
 
-  componentDidMount(){
-    setTimeout( () =>{
-      this.setState({
-        isUpdate:false,
-      })
-    }, 5000);
+  componentWillMount() {
+    this._checkForUpdate();
   }
-  codePushStatusDidChange(status) {
 
-      switch(status) {
-          case CodePush.SyncStatus.CHECKING_FOR_UPDATE:
-              console.log("Checking for updates.");
-              break;
-          case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
-              console.log("Downloading package.");
-              this.setState({
-                isUpdate:true,
-              })
-              break;
-          case CodePush.SyncStatus.INSTALLING_UPDATE:
-              console.log("Installing update.");
-              break;
-          case CodePush.SyncStatus.UP_TO_DATE:
-              console.log("Up-to-date.");
-              break;
-          case CodePush.SyncStatus.UPDATE_INSTALLED:
-              console.log("Update installed.");
-              break;
-      }
+  _checkForUpdate(){
+    CodePush.checkForUpdate(DEPLOYMENT_KEY).then((update) => {
+      if (!update) {
+        console.log("[CodePush] The app is up to date!");
+        this.setState({isUpdating: false});
+      } else {
+        console.log("[CodePush] Found new update");
+        this.setState({isUpdating: true});
+
+        CodePush.sync({
+          deploymentKey: DEPLOYMENT_KEY,
+          checkFrequency: CodePush.CheckFrequency.ON_APP_START,
+          installMode: CodePush.InstallMode.IMMEDIATE
+        }, (status) => {
+          if (status == 2 || status == 3 || status == 7){
+            //  Check status code on https://docs.microsoft.com/en-us/appcenter/distribution/codepush/react-native#api-reference
+            this.setState({isUpdating: true});
+          }
+          else{
+            this.setState({isUpdating: false});
+          }
+        })
+      }});
   }
-  _renderUpdateView(){
-    return(
-      <View style={{position:'absolute',top:0,left:0,right:0,bottom:0,backgroundColor:'#ffffff',alignItems:'center',justifyContent:'center',}}>
-        <Image source={require('./App/Image/Loading_dots_orange.gif')}  style={{width:45,height:15}}/>
-      </View>
-    )
+  
+  _renderUpdateView() {
+    return (<View style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: '#ffffff',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+      <Image source={require('./App/Image/Loading_dots_orange.gif')} style={{
+          width: 45,
+          height: 15
+        }}/>
+    </View>)
   }
 
   render() {
-      return (
-        <View style={{flex:1}}>
-           <App />
-          {this.state.isUpdate? this._renderUpdateView():null}
-        </View>
-      );
+    return (
+      <View style={{flex: 1}}>
+        <App/>
+        {this.state.isUpdating ? this._renderUpdateView() : null}
+      </View>
+  );
   }
 }
-let  codePushOptions = {checkFrequency: CodePush.CheckFrequency.ON_APP_START,
-                        installMode: CodePush.InstallMode.IMMEDIATE};
-cmDriver = CodePush(codePushOptions)(cmDriver);
